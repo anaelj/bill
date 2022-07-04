833c0fe014c926703628ea4a1d1cb81e215bf35d
Anael Medeiros <anael.medeiros@compasso.com.br>
2022-07-04 17:01:41 -0400

    add rotinas de foto

--- src/pages/ExpenseItem/index.tsx
+++ src/pages/ExpenseItem/index.tsx
@@ -1,6 +1,6 @@
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import { Row, Col, Form } from "react-bootstrap";
+import { Row, Col, Form, Button } from "react-bootstrap";
 
 import { Container, MainContainer } from "./styles";
 import { MdLocalShipping, MdCheck, MdAddAPhoto } from "react-icons/md";
@@ -11,10 +11,23 @@ import { useCallback, useRef, useState } from "react";
 
 export const ExpenseItem = () => {
   const videoConstraints = {
-    width: 1280,
-    height: 720,
-    facingMode: "user",
+    facingMode: { exact: "environment" },
+    // facingMode: "user",
   };
+
+  const webcamRef = useRef<Webcam>(null);
+  const [imageFromCamera, setImageFromCamera] = useState("");
+
+  const capture = useCallback(() => {
+    if (webcamRef) {
+      console.log(webcamRef);
+      const imageFromCamera = webcamRef?.current?.getScreenshot();
+      if (imageFromCamera) {
+        setShowCamera(false);
+        setImageFromCamera(imageFromCamera);
+      }
+    }
+  }, [webcamRef]);
 
   const [showCamera, setShowCamera] = useState(false);
 
@@ -26,36 +39,51 @@ export const ExpenseItem = () => {
         label={"Item de Despesa"}
       />
       <Container>
-        <Form.Group className="mb-3">
-          <Form.Label>Descrição</Form.Label>
-          <Form.Control type="text" placeholder="Breve descrição" />
-          <Form.Label>Quantidade</Form.Label>
-          <Form.Control type="text" placeholder="Quantidade" />
-          <Form.Label>Valor Total</Form.Label>
-          <Form.Control type="text" placeholder="Valor Total" />
-          <Form.Label>Categoria</Form.Label>
-          <Form.Select>
-            {expenseCategory.map((category) => (
-              <option>{category.name}</option>
-            ))}
-          </Form.Select>
-        </Form.Group>
-        <Row
-          style={{
-            width: "100%",
-            height: "200px",
-            border: "1px dashed black",
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          {showCamera ? (
-            <Webcam />
-          ) : (
-            <MdAddAPhoto size={150} onClick={() => setShowCamera(true)} />
-          )}
-        </Row>
+        {!showCamera && (
+          <>
+            <Form.Group className="mb-3">
+              <Form.Label>Descrição</Form.Label>
+              <Form.Control type="text" placeholder="Breve descrição" />
+              <Form.Label>Quantidade</Form.Label>
+              <Form.Control type="text" placeholder="Quantidade" />
+              <Form.Label>Valor Total</Form.Label>
+              <Form.Control type="text" placeholder="Valor Total" />
+              <Form.Label>Categoria</Form.Label>
+              <Form.Select>
+                {expenseCategory.map((category) => (
+                  <option>{category.name}</option>
+                ))}
+              </Form.Select>
+            </Form.Group>
+            <Row
+              style={{
+                width: "100%",
+                border: "1px dashed black",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              {imageFromCamera !== "" ? (
+                <img src={imageFromCamera} alt={""} />
+              ) : (
+                <MdAddAPhoto size={150} onClick={() => setShowCamera(true)} />
+              )}
+            </Row>
+          </>
+        )}
+        {showCamera && (
+          <div style={{ maxWidth: "90%" }}>
+            <Webcam
+              audio={false}
+              ref={webcamRef}
+              width={350}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+            />
+            <Button onClick={capture}>Capturar Imagem</Button>
+          </div>
+        )}
       </Container>
       <Footer>
         <Row style={{ width: "100%" }}>
