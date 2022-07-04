1b2daa81dd4d605a57f7a429ec6def7b05dfbc2e
Anael Medeiros <anael.medeiros@compasso.com.br>
2022-07-04 07:49:01 -0400

    add path icons

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,55 +1,48 @@
 export enum EnumStatusTravel {
-  finished = 'F',
-  inProgress = 'I'
+  finished = "F",
+  inProgress = "I",
 }
-export const travels= [{
-  number: 1,
-  origin: "Campo Grande - MS",
-  destiny: "Dourados - MS",
-  status: EnumStatusTravel.inProgress
-
-},{
-  number: 2,
-  origin: "Campo Grande - MS",
-  destiny: "Bonito - MS",
-  status: EnumStatusTravel.finished
-
-},{
-  number: 3,
-  origin: "Corumbá - MS",
-  destiny: "Rio Verde - MS",
-  status: EnumStatusTravel.finished
-
-},
-{
-  number: 4,
-  origin: "Sidrolândia - MS",
-  destiny: "Camapuã - MS",
-  status: EnumStatusTravel.finished
-
-}
-,
-{
-  number: 5,
-  origin: "Campo Grande - MS",
-  destiny: "Sidrolândia - MS",
-  status: EnumStatusTravel.finished
-
-}
-,
-{
-  number: 6,
-  origin: "Sidrolândia - MS",
-  destiny: "Dourados - MS",
-  status: EnumStatusTravel.finished
-
-}
-,
-{
-  number: 7,
-  origin: "Jardim - MS",
-  destiny: "Dourados - MS",
-  status: EnumStatusTravel.finished
-
-}
-]
+export const travels = [
+  {
+    number: 7,
+    origin: "Campo Grande - MS",
+    destiny: "Dourados - MS",
+    status: EnumStatusTravel.inProgress,
+  },
+  {
+    number: 6,
+    origin: "Campo Grande - MS",
+    destiny: "Bonito - MS",
+    status: EnumStatusTravel.finished,
+  },
+  {
+    number: 5,
+    origin: "Corumbá - MS",
+    destiny: "Rio Verde - MS",
+    status: EnumStatusTravel.finished,
+  },
+  {
+    number: 4,
+    origin: "Sidrolândia - MS",
+    destiny: "Camapuã - MS",
+    status: EnumStatusTravel.finished,
+  },
+  {
+    number: 3,
+    origin: "Campo Grande - MS",
+    destiny: "Sidrolândia - MS",
+    status: EnumStatusTravel.finished,
+  },
+  {
+    number: 2,
+    origin: "Sidrolândia - MS",
+    destiny: "Dourados - MS",
+    status: EnumStatusTravel.finished,
+  },
+  {
+    number: 1,
+    origin: "Jardim - MS",
+    destiny: "Dourados - MS",
+    status: EnumStatusTravel.finished,
+  },
+];
